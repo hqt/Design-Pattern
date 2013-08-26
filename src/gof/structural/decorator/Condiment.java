17c33052d0796e263c9625a3016456abcd53aa0a
Huynh Quang Thao <huynhquangthao@gmail.com>
2013-08-27 03:01:21 +0700

    decorator edit : move all java class files to one related file

--- src/gof/structural/decorator/Condiment.java
+++ src/gof/structural/decorator/Condiment.java
@@ -10,3 +10,47 @@ public abstract class Condiment implements IBeverage {
 		this.iBeverage = iBeverage;
 	}
 }
+
+/**
+* is ConcreteDecorator in UML Diagram
+* is a concrete class, implement decorate function for reference object
+* @author Huynh Quang Thao
+*/
+
+class IceCondiment extends Condiment {
+
+	public IceCondiment(IBeverage iBeverage) {
+		super(iBeverage);
+	}
+
+	@Override
+	public double cost() {
+		return iBeverage.cost() + 1;
+	}
+
+	@Override
+	public void ingredient() {
+		iBeverage.ingredient();
+		System.out.println("Add Ice");
+	}
+}
+
+class MilkCondiment extends Condiment {
+
+	public MilkCondiment(IBeverage iBeverage) {
+		super(iBeverage);
+	}
+
+	@Override
+	public double cost() {
+		return iBeverage.cost() + 3;
+	}
+
+	@Override
+	public void ingredient() {
+		iBeverage.ingredient();
+		System.out.println("Add Milk");
+	}
+
+}
+
--- src/gof/structural/decorator/IBeverage.java
+++ src/gof/structural/decorator/IBeverage.java
@@ -6,3 +6,38 @@ public interface IBeverage {
 	public double cost();
 	public void ingredient();
 }
+
+/**
+ * this is concrete class
+ * in UML is Concrete Component : is a concrete class that need to be decorate
+ */
+
+class Coffee implements IBeverage {
+
+	@Override
+	public double cost() {
+		return 10;	// cost of a normal coffee
+	}
+
+	@Override
+	public void ingredient() {
+		System.out.println("Add Coffee");
+	}
+}
+
+class Tea implements IBeverage {
+
+	@Override
+	public double cost() {
+		return 5;	// cost of normal tea
+	}
+
+	@Override
+	public void ingredient() {
+		System.out.println("Add Sugar");
+		System.out.println("Add Tea Leaf");
+	}
+}
+
+
+
